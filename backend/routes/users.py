ab776faa9c6e660c7f8bdaa4f1ee39e9de93189f
Sagehuang <Sagehuang3000@gmail.com>
2025-05-13 11:50:06 +0800

    docs(users): update example in routes/users.py

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -5,6 +5,47 @@ from models.group import Group
 
 users_bp = Blueprint("users", __name__, url_prefix="/api/users")
 
+"""
+這裡寫了根據 API 文件開發的前兩個 route, 並附上如何測試
+
+# 範例一
+@users_bp.route("/signin", methods=["POST"])
+def sign_in():
+    data = request.get_json()
+    name = data.get("name")
+
+    # 嘗試找出是否已有此使用者
+    user = User.query.filter_by(name=name).first() # 若已有此使用者 return user object; 若無此使用者 return None
+
+    # 若無此使用者，則新增此使用者
+    if not user:
+        user = User(name=name)  # 新增一個 User object
+        db.session.add(user)  # 將新使用者加入到資料庫
+        db.session.commit()  # 提交更改到資料庫
+
+    return jsonify(user.to_dict()), 200
+
+測試:
+backend app run 起來的情況下另外開一個 terminal
+curl -X POST http://localhost:5001/api/users/signin \
+     -H "Content-Type: application/json" \
+     -d '{"name": "Alice"}'
+terminal 會印出 backend app 的 response
+
+# 範例二
+@users_bp.route("/<int:user_id>", methods=["GET"])
+def get_user_detail(user_id):  # 上面用 <> 包起來的東西可以作為參數傳進函數裡
+    user = User.query.get(user_id)
+    
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    return jsonify(user.to_dict()), 200
+
+測試:
+curl -X GET http://localhost:5001/api/users/1
+"""
+
 # 新增 group:
 @users_bp.route("/signin", methods=["POST"])
 def sign_in():
