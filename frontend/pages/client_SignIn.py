c1f7850c356a5227716d721721e2ccc725c827cc
Nien-YuChang <vlntnchny@proton.me>
2025-05-18 13:59:53 +0800

    純粹改掉換行

--- frontend/pages/client_SignIn.py
+++ frontend/pages/client_SignIn.py
@@ -14,38 +14,29 @@ class HomePage(ctk.CTkFrame):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)  # 上下平均分布一點空間
 
-        font_title = ctk.CTkFont(family='Gotham', size=24,
-                                        weight='bold')
+        font_title = ctk.CTkFont(family='Gotham', size=24, weight='bold')
         small_font = ctk.CTkFont(family='Gotham', size=12)
         # 標題
-        self.title_label = ctk.CTkLabel(self, text='Group Expense Tracker',
-                                        font=font_title)
+        self.title_label = ctk.CTkLabel(self, text='Group Expense Tracker', font=font_title)
 
         # 放 label + entry 的內部框架，方便組合左上角，並使用 grid 排版
         self.input_frame = ctk.CTkFrame(self, fg_color='transparent')
         self.input_frame.grid_columnconfigure(0, weight=1)
 
-        self.name_label = ctk.CTkLabel(self.input_frame, text='Name',
-                                       font=small_font, anchor='w')
+        self.name_label = ctk.CTkLabel(self.input_frame, text='Name', font=small_font, anchor='w')
         self.name_entry = ctk.CTkEntry(self.input_frame, width=200)
 
         # Sign In 按鈕
-        self.sign_in_button = ctk.CTkButton(self, width=200, text='Sign In',
-                                            font=small_font,
-                                            command=self.handle_sign_in)
+        self.sign_in_button = ctk.CTkButton(self, width=200, text='Sign In', font=small_font, command=self.handle_sign_in)
 
         # 結果文字
-        self.result_label = ctk.CTkLabel(self, width=200, text='',
-                                         font=small_font)
+        self.result_label = ctk.CTkLabel(self, width=200, text='', font=small_font)
 
         # 在 grid 中放入元件
-        self.title_label.grid(row=0, column=0, padx=15, pady=(40, 20),
-                              sticky='nsew')
+        self.title_label.grid(row=0, column=0, padx=15, pady=(40, 20), sticky='nsew')
         self.input_frame.grid(row=1, column=0, sticky='nsew')
-        self.name_label.grid(row=0, column=0, sticky='nsew',
-                             padx=(15, 0), pady=(0, 2))
-        self.name_entry.grid(row=1, column=0, padx=15, pady=(0, 20),
-                             sticky='nsew')
+        self.name_label.grid(row=0, column=0, sticky='nsew', padx=(15, 0), pady=(0, 2))
+        self.name_entry.grid(row=1, column=0, padx=15, pady=(0, 20), sticky='nsew')
         self.sign_in_button.grid(row=2, column=0, padx=15, sticky='nsew')
         self.result_label.grid(row=3, column=0, padx=15, sticky='nsew')
 
