af8629e33457c51a39d8e5ac567daab4fd29789d
Nien-YuChang <vlntnchny@proton.me>
2025-05-18 14:21:40 +0800

    先將叫出首頁的程式碼寫入

--- frontend/app.py
+++ frontend/app.py
@@ -1,3 +1,18 @@
 import customtkinter as ctk
 import datetime
 import api_client
+
+from pages.client_SignIn import HomePage
+
+ctk.set_appearance_mode('System')
+ctk.set_default_color_theme('blue')
+
+if __name__ == '__main__':
+    app = ctk.CTk()
+    app.geometry('400x640')
+    app.title('Group Expense Tracker')
+
+    home_page = HomePage(app)
+    home_page.pack(expand=True)
+
+    app.mainloop()
