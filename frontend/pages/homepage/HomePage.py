5fa81ce5af558070306ff707fd1b0706d6da93ce
Nien-YuChang <vlntnchny@proton.me>
2025-05-23 00:02:44 +0800

    HomePage.py 新增 load_groups method

--- frontend/pages/homepage/HomePage.py
+++ frontend/pages/homepage/HomePage.py
@@ -17,65 +17,63 @@ class HomePage(ctk.CTkFrame):
         self.grid_rowconfigure(2, weight=0)  # 底部按鈕高度固定
         self.grid_columnconfigure(0, weight=1)
 
-        font_top_bar = ctk.CTkFont(family='Gotham', size=20, weight='bold')
-        small_font = ctk.CTkFont(family='Gotham', size=12)
+        # Pre-defined fonts
+        self.font_top_bar = ctk.CTkFont(family='Gotham', size=20, weight='bold')
+        self.small_font = ctk.CTkFont(family='Gotham', size=12)
 
-        # 頂端列
-        top_bar = ctk.CTkFrame(self, fg_color='#F3F6F4')
-        top_bar.grid(row=0, column=0, sticky='ew')
-        top_bar.grid_columnconfigure(0, weight=0)
-        top_bar.grid_columnconfigure(1, weight=1)
-        top_bar.grid_columnconfigure(2, weight=0)
+        # 頂端列設置
+        self.top_bar = ctk.CTkFrame(self, fg_color='#F3F6F4')
+        self.top_bar.grid(row=0, column=0, sticky='ew')
+        self.top_bar.grid_columnconfigure(0, weight=0)
+        self.top_bar.grid_columnconfigure(1, weight=1)
+        self.top_bar.grid_columnconfigure(2, weight=0)
 
-        self.home_button = ctk.CTkButton(top_bar, text='Home', font=small_font, command=self.on_navigate_home, width=80)
-        self.title_label = ctk.CTkLabel(top_bar, text='My Group', text_color='black', font=font_top_bar)
-        self.logout_button = ctk.CTkButton(top_bar, text='Logout', font=small_font, command=self.on_logout, width=80)
+        # 設定頂端列的物件
+        self.home_button = ctk.CTkButton(self.top_bar, text='Home', font=self.small_font, command=self.on_navigate_home, width=80)
+        self.title_label = ctk.CTkLabel(self.top_bar, text='My Group', text_color='black', font=self.font_top_bar)
+        self.logout_button = ctk.CTkButton(self.top_bar, text='Logout', font=self.small_font, command=self.on_logout, width=80)
 
+        # 將物件放入頂端列
         self.home_button.grid(row=0, column=0, padx=10, pady=10, sticky='w')
         self.title_label.grid(row=0, column=1, padx=10, pady=10, sticky='ew')
         self.logout_button.grid(row=0, column=2, padx=10, pady=10, sticky='e')
 
-        # 群組假資料
-        '''
-        self.current_groups = [{'id': 1, 'name': 'Group A', 'members': [1, 2, 4]},
-                               {'id': 2, 'name': 'Group B', 'members': [1, 3, 4]},
-                               {'id': 3, 'name': 'Group C', 'members': [1, 9, 10]},
-                               {'id': 4, 'name': 'Group D', 'members': [1, 8, 23]}]
-        '''
+        # 主體捲動區設置
+        self.scrollable = ctk.CTkScrollableFrame(self)
+        self.scrollable.grid(row=1, column=0, sticky='nsew', padx=10, pady=(0, 10))
+        self.scrollable.grid_columnconfigure(0, weight=1)
 
-        try:  # !!!
+        # 底部列設置
+        self.bottom_frame = ctk.CTkFrame(self, fg_color='transparent')
+        self.bottom_frame.grid(row=2, column=0, sticky='ew', padx=10, pady=10)
+        self.bottom_frame.grid_columnconfigure(0, weight=1)
+        self.bottom_frame.grid_columnconfigure(1, weight=1)
+
+        # 設定底部的按鈕
+        self.add_group_button = ctk.CTkButton(self.bottom_frame, text='Add Group', font=self.small_font, command=self.on_add_group)
+        self.join_group_button = ctk.CTkButton(self.bottom_frame, text='Join Group', font=self.small_font, command=self.on_join_group)
+
+        # 將物件放入底部列
+        self.add_group_button.grid(row=0, column=0, padx=10, sticky='ew')
+        self.join_group_button.grid(row=0, column=1, padx=10, sticky='ew')
+
+    # 從後端抓取資料；須在 app.py 中呼叫頁面時先行叫出此 method
+    def load_groups(self):  # !!!
+        try:
             self.current_groups = get_groups_info(self.controller.user_id)
             print(f'API 回傳：{self.current_groups}')
         except Exception as error:
             print(f'API 發生錯誤：{error}')
 
-        # 主體捲動區
-        scrollable = ctk.CTkScrollableFrame(self)
-        scrollable.grid(row=1, column=0, sticky='nsew', padx=10, pady=(0, 10))
-        scrollable.grid_columnconfigure(0, weight=1)
-
         if self.current_groups:
             for group in self.current_groups:
-                group_button = ctk.CTkButton(scrollable, text=group['group_name'], text_color='#FFFFFF', font=small_font,
-                                             border_width=1, border_color='#B0B0B0',
-                                             fg_color='#B0B0B0',
+                group_button = ctk.CTkButton(self.scrollable, text=group['group_name'], text_color='#FFFFFF', font=self.small_font,
+                                             border_width=1, border_color='#B0B0B0', fg_color='#B0B0B0',
                                              command=lambda g=group: self.check_group(g))
                 group_button.pack(fill='x', padx=5, pady=5)
         else:
-            no_group_label = ctk.CTkLabel(scrollable, text='You are not in any group for now. :(\nWhy not try adding or joining a group?', font=small_font)
-            no_group_label.pack(pady=20)
-
-        # 底部按鈕
-        bottom_frame = ctk.CTkFrame(self, fg_color='transparent')
-        bottom_frame.grid(row=2, column=0, sticky='ew', padx=10, pady=10)
-        bottom_frame.grid_columnconfigure(0, weight=1)
-        bottom_frame.grid_columnconfigure(1, weight=1)
-
-        self.add_group_button = ctk.CTkButton(bottom_frame, text='Add Group', font=small_font, command=self.on_add_group)
-        self.join_group_button = ctk.CTkButton(bottom_frame, text='Join Group', font=small_font, command=self.on_join_group)
-
-        self.add_group_button.grid(row=0, column=0, padx=10, sticky='ew')
-        self.join_group_button.grid(row=0, column=1, padx=10, sticky='ew')
+            self.no_group_label = ctk.CTkLabel(self.scrollable, text='You are not in any group for now. :(\nWhy not try adding or joining a group?', font=self.small_font)
+            self.no_group_label.pack(pady=20)
 
     # 頁面切換
     def on_navigate_home(self):
@@ -85,8 +83,8 @@ class HomePage(ctk.CTkFrame):
         self.show_page('SignIn')
 
     def check_group(self, group):
-        self.controller.clicked_group_id = group['group_id']
         print('Checking the group with ID {self.controller.clicked_group_id}...')
+        self.controller.clicked_group_id = group['group_id']
         self.show_page('ViewGroup')
 
     def on_add_group(self):
